13b8445d2e035d5d2ca0475edc20aae7e20c7125
raghavkumar <ray.kumar91@gmail.com>
2014-12-18 00:03:35 -0500

    Changes dec 17 2014

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -1,7 +1,17 @@
 # Django settings for tango_with_django_project project.
+import os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
+
+SETTINGS_DIR = os.path.dirname(__file__)
+
+PROJECT_PATH = os.path.join(SETTINGS_DIR, os.pardir)
+
+PROJECT_PATH = os.path.abspath(PROJECT_PATH)
+TEMPLATE_PATH = os.path.join(PROJECT_PATH, 'templates')
+
+STATIC_PATH = os.path.join(PROJECT_PATH, 'static')
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
@@ -72,6 +82,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    STATIC_PATH,
 )
 
 # List of finder classes that know how to find static files in
@@ -111,6 +122,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    TEMPLATE_PATH,
 )
 
 INSTALLED_APPS = (
